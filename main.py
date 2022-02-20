be01b85ae70c68c66616c7855912d1dc1ad6337f
Thatsaphon <thatphon123@hotmail.com>
2022-02-20 20:49:28 +0700

    connect atlas when start app

--- main.py
+++ main.py
@@ -8,39 +8,36 @@ from db import *
 
 app = FastAPI()
 
+connect_atlas()
 
-@app.get("/")
+
+@app.get("/book/")
 def get_all_books():
-    connect_atlas()
     books = get_books_from_mongo()
     return {
         "books": books
     }
 
 
-@app.get("/{book_title}")
+@app.get("/book/{book_title}")
 def get_book_from_title(book_title: str):
-    connect_atlas()
     book = get_book_from_mongo(book_title)
     return book
 
 
-@app.post("/", status_code=201)
+@app.post("/book/", status_code=201)
 def create_new_book(book: BookIn):
-    connect_atlas()
     create_new_book_in_mongo(book)
     return book
 
 
-@app.put("/{book_title}")
+@app.put("/book/{book_title}")
 def update_book(book_title: str, book: BookIn):
-    connect_atlas()
     update_book_in_mongo(book_title, book)
     return
 
 
-@app.delete("/{book_title}")
+@app.delete("/book/{book_title}")
 def delete_book(book_title: str):
-    connect_atlas()
     delete_book_in_mongo(book_title)
     return
